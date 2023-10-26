8429522d0140882b928053ed0ce16e4438485d51
Ki-won <seokw2240@gmail.com>
2023-10-26 15:10:28 +0900

    add userCheck_proto

---
+++ Model/DatabaseNonAut.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaStory.Model
+{
+    class DatabaseNonAut
+    {
+        class UserFunction
+        {
+            private string Id;
+            private string PW;
+
+            public int UesrCheck(String Id, String Pw)
+            {
+                string Query = "SELECT * FROM Member WHERE ID = @ID AND PW = @PW; ";
+                `
+                return 20231026;
+
+            }
+
+        }
+    }
+}
