30a5f6b8f58714f4fa2cbab5a4c36c65268fb6c2
mokoon <lghj990724@gmail.com>
2023-11-07 21:41:24 +0900

    11-07
    나머지는 DB 함수

--- UI/user_interface_main.cs
+++ UI/user_interface_main.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static SeaStory.Model.DataCalss;
+using User = SeaStory.Model.DataCalss.User;
 
 namespace SeaStory
 {
     public partial class user_interface_main : Form
     {
-        public user_interface_main()
+        public user_interface_main(string ID, int user_type, string seat_number)
         {
-             InitializeComponent();
+            InitializeComponent();
+            //회원 로그인
+            if (user_type == 0)
+            {
+                //로그인 폼에서 받아온 ID 기반 유저 정보 넣기
+                User user = Model.DatabaseAut.UserData(ID);
+
+                //유저 정보 기반 유저명 호출 및 표시
+                label3.Text = user.Name;
+                //남은 시간
+                label6.Text = user.Time;
+            }
+            if (user_type == 1)
+            {
+                //비회원 로그인
+            }
+            label8.Text = seat_number;
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
--- UI/user_seat.cs
+++ UI/user_seat.cs
@@ -11,6 +11,7 @@ using Microsoft.VisualBasic.ApplicationServices;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using static SeaStory.Model.DataCalss;
 using User = SeaStory.Model.DataCalss.User;
+using static SeaStory.Model.DatabaseNonAut;
 
 namespace SeaStory.UI
 {
@@ -18,9 +19,16 @@ namespace SeaStory.UI
     {
         //user_type = 0 회원 유저
         //user_type = 1 비회원 유저
+
+        private string userID;
+        private int userType;
+        private string selectedSeat;
+
         public user_seat(string ID,int user_type)
         {
             InitializeComponent();
+            userID = ID;
+            userType = user_type;
             //회원 로그인
             if (user_type == 0)
             {
@@ -38,7 +46,7 @@ namespace SeaStory.UI
             }
 
             //잔여석 사용중 좌석 표시
-
+            //List<Seat> seats = Model.DatabaseNonAut.GetSeats();
 
 
         }
@@ -56,10 +64,12 @@ namespace SeaStory.UI
         private void button1_Click(object sender, EventArgs e)
         {
             //1.해당 좌석에 ID기반으로 DB에 사용 유저와 적립 시간을 넣는다
-            //2.유저 ID, 좌석의 번호를 main폼으로 넘긴다
-            user_interface_main user_Interface_main = new user_interface_main();
+            //2.유저 ID, 회원 비회원 토글 ,좌석의 번호를 main폼으로 넘긴다
+            //임의로 seat = "01" 로 생성해서 테스트
+            string seat = "01";
+            user_interface_main user_Interface_main = new user_interface_main(userID, userType, seat);
             user_Interface_main.Show();
-            //this.Close ();
+            this.Close ();
         }
     }
 }
