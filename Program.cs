a02024495f890875c79f2a05cce6294177a89dd5
leehj67 <109390172+leehj67@users.noreply.github.com>
2023-11-14 10:09:43 +0900

    ui_tts
    tts추가

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using SeaStory.UI.tts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,7 @@ namespace SeaStory
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new login());
+            Application.Run(new tts_test());
         }
     }
 }
