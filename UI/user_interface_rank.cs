3819c22d107ac2d899796e28e8f9e1b7cc962979
Ki_won <seokw2240@gmail.com>
2023-11-23 15:27:43 +0900

    랭킹보기 구현완료

--- UI/user_interface_rank.cs
+++ UI/user_interface_rank.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SeaStory.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static SeaStory.Model.DataCalss;
 
 namespace SeaStory.UI
 {
@@ -15,11 +17,34 @@ namespace SeaStory.UI
         public user_interface_rank()
         {
             InitializeComponent();
+            list_Update();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             //유저 랭킹 리스트 형태로 받아오는 함수 필요
+            list_Update();
         }
+
+        private void list_Update()
+        {
+            listView1.Items.Clear();
+            listView1.Columns.Clear();
+            listView1.Columns.Add("아이디", 200, HorizontalAlignment.Center);
+            listView1.Columns.Add("유저명", 200, HorizontalAlignment.Center);
+            listView1.Columns.Add("사용시간", 250, HorizontalAlignment.Center);
+            List<User2> users = DatabaseAut.GetRanking();
+
+            // listView1에 주문 목록 추가
+            foreach (User2 userss in users)
+            {
+                ListViewItem item = new ListViewItem(userss.ID);
+                item.SubItems.Add(userss.Name);
+                item.SubItems.Add(userss.UsedTime.ToString());
+
+                listView1.Items.Add(item);
+            }
+        }
+
     }
 }
